2eeb001f0446e73b2fceae48df62359f1803d1ae
khaledgamal22 <kghaly56@gmail.com>
2025-02-25 14:50:31 +0200

    child in tween builder

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,26 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:tween_animation_builder/second_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  double _begin = 100;
+
+  double _end = 200;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tween Animation Builder'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SecondView()),
-            );
-          },
-          child: const Text('Click'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tween Animation Builder'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              buildAnimatedContainer(),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  _begin = _begin == 100 ? 200 : 100;
+                  _end = _end == 200 ? 100 : 200;
+                  setState(() {});
+                },
+                child: const Text('Click'),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  TweenAnimationBuilder<double> buildAnimatedContainer() {
+    return TweenAnimationBuilder(
+        tween: Tween<double>(begin: _begin, end: _end),
+        duration: const Duration(seconds: 1),
+        child: Column(
+          children: [
+            Spacer(),
+            Text(
+              'child',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            Spacer(),
+            Text(
+              'child',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            Spacer(),
+          ],
+        ),
+        builder: (context, value, child) {
+          return Container(
+            width: value,
+            height: value,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: child,
+          );
+        });
   }
 }
